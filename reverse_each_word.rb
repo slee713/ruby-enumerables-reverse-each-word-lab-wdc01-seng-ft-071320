3b867b120afc16db9ae09a03f194e6d26b509d27
Steven Lee <steven.lee1393@gmail.com>
2020-07-03 22:19:47 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(string)
+#   array = string.split()
+#   reverse_array=[]
+#   array.each do |element|
+#     reverse_array.push(element.reverse)
+#   end
+#   reverse_array.join(" ")
+# end
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect do |element|
+    element.reverse!
+  end
+  array.join(" ")
+end
