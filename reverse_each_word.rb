a133bb640504c5ee3f358ea12b916de910cf90d6
Steven Lee <steven.lee1393@gmail.com>
2020-07-03 22:08:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split()
   reverse_array=[]
   array.each do |element|
-    reverse_array.push(array[element].reverse)
+    reverse_array.push(element.reverse)
   end
   reverse_array
 end
